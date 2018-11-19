1aa4b360561bc008afc932023f294c924e31fa0d
tdsat <tdspartakos@gmail.com>
2018-11-19 19:44:54 +0200

    new model functions

--- upload/admin/model/tw/orderslive.php
+++ upload/admin/model/tw/orderslive.php
@@ -1,7 +1,7 @@
 <?php
 
 class ModelTwOrderslive extends Model{
-	public function getOrdersAfter($order_id = NULL) {
+	public function getOrdersAfterOrderId($order_id = NULL) {
 		if($order_id){
 		$sql = "SELECT o.order_id, o.date_added, o.date_modified
 			FROM `" . DB_PREFIX . "order` o
@@ -16,5 +16,25 @@ class ModelTwOrderslive extends Model{
 		}
 		$query = $this->db->query($sql);
 		return $query->rows;
+	}
+
+	public function getOrdersAfter($timestamp = 0){
+		if(!$timestamp){
+			return $this->getLatestOrder();
+		}
+
+		$query = "SELECT o.order_id, o.date_added, o.date_modified
+			FROM `" . DB_PREFIX . "order` o
+			WHERE o.order_status_id > '0'
+			AND `date_added` >= DATE($timestamp) OR `date_modified` >= DATE($timestamp)";
+		return $this->db->query($query)->rows;
+	}
+
+	public function getLatestOrder(){
+		return $this->db->query("SELECT o.order_id, o.date_added, o.date_modified,GREATEST(o.date_added, o.date_modified) as `date_changed`
+			FROM `" . DB_PREFIX . "order` o
+			WHERE o.order_status_id > '0'
+			ORDER BY `date_changed` DESC
+			LIMIT 1")->rows[0];
 	}
 }
